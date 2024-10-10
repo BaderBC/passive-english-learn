68ee54dd2be0941a57d46ebf04ffbb09be9f075f
Bartłomiej Strama <bstrama@bstrama.com>
2024-10-10 23:36:14 +0200

    panic of failiture

--- generate_merula_mp3.py
+++ generate_merula_mp3.py
@@ -2,6 +2,11 @@ import json
 import os
 import subprocess
 
+def run_command(command):
+    result = subprocess.run(command)
+    if result.returncode != 0:
+        raise RuntimeError(f"Command {command} failed with return code {result.returncode}")
+
 merula_json = open("merula.json").read()
 merula_resources = json.loads(merula_json)
 
@@ -11,10 +16,11 @@ for focus, chapters in merula_resources.items():
         os.makedirs(output_dir, exist_ok=True)
 
         # scrap merula into input.json
-        subprocess.run(["python", "scrap_merula_pl.py", url])
+        run_command(["python", "scrap_merula_pl.py", url])
 
         # generate mp3 from input.json
-        subprocess.run(["python", "generate_mp3.py", output_dir])
+        run_command(["python", "generate_mp3.py", output_dir])
 
         # print in red color:
         print(f"\n\n\033[91m{focus} {chapter} generated!\033[0m\n\n")
+
